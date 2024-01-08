3c6088e2d15188a8b03523d144e5198b89416171
sandeep <sandyk1536@gmail.com>
2024-01-08 13:44:44 +0530

    Added base files

---
+++ src/components/TodoInput.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+
+function TodoInput(props) {
+    const [inputText, setInputText] = useState("")
+    const handelEnterPress = (e)=>{
+        if(e.keyCode === 13){
+        props.addList(inputText)
+        setInputText("")
+        }
+    }
+  return (
+    <div className="input-container">
+        <input type="text" className="input-box-todo" placeholder='Enter Your todo' value = {inputText}
+        onChange={e=>{setInputText(e.target.value)}} onKeyDown={handelEnterPress}/>
+        <button className="add-btn" onClick = {()=>{props.addList(inputText);setInputText(inputText)}}>+</button>
+        
+    </div>
+  )
+}
+
+export default TodoInput
